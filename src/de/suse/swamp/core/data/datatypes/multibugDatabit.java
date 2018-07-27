ad684aa1214402e0e1f045d796459c38e6d0b8ad
Carsten Hoeger <choeger@open-xchange.com>
2018-07-27 08:03:29 +0200

    change multibug databit from numberDatabit to Databit in order to be able to use regex in events

--- src/de/suse/swamp/core/data/datatypes/multibugDatabit.java
+++ src/de/suse/swamp/core/data/datatypes/multibugDatabit.java
@@ -26,14 +26,8 @@
 
 package de.suse.swamp.core.data.datatypes;
 
-import java.util.*;
-
-import org.apache.jcs.access.exception.*;
-import org.xml.sax.*;
-
-import de.suse.swamp.core.container.*;
-import de.suse.swamp.core.data.*;
-import de.suse.swamp.core.util.*;
+import org.apache.jcs.access.exception.InvalidArgumentException;
+import de.suse.swamp.core.data.Databit;
 
 /**
  * The Databit
@@ -42,54 +36,22 @@ import de.suse.swamp.core.util.*;
  *
  */
 
-public class multibugDatabit extends numberDatabit {
-    
+public class multibugDatabit extends Databit {
+
 
     public multibugDatabit(String name, String desc, String value, Integer state) throws Exception {
-        super (name, desc, value, state);
+        super (name, desc, value, state.intValue());
         setModified(true);
     }
-    
+
     /**
      * Checks if the given value v fits this Databit's Type.
      * 
      * @param v
      */
     public String checkDataType(String v) throws InvalidArgumentException, Exception {
-        // empty values are allowed
-        if (!v.equals("") && !checkedValues.contains(v)) {
-	    
-            // datatype person may be a comma-separated
-            // email, or loginname list
-            StringTokenizer st = new StringTokenizer(v, ",");
-            Dataset dset = null;
-            // perform basic type checking
-            // if this is a new databit, we may not have a dataset yet.
-            if (getId() > 0){
-        	dset = getDataset();
-            }
-            while (st.hasMoreTokens()) {
-                String tokenvalue = st.nextToken().trim();
-
-		try {
-		    BugzillaTools bTools = new BugzillaTools();
-		    bTools.fetchBugzillaInfo(dset, new Integer(tokenvalue).intValue());
-		} catch (SAXParseException spe) {
-		    throw new InvalidArgumentException(i18n.tr("Unable to YYYY fetch data from: ") + 
-						       SWAMP.getInstance().getProperty("BUGZILLA_QUERYURL") + tokenvalue + "\n" + 
-						       i18n.tr("Parsing error in line ") + spe.getLineNumber() + i18n.tr(", column ") + spe.getColumnNumber() + "\n" + 
-						       i18n.tr("Reason: ") + spe.getMessage());
-		} catch (Exception e) {
-		    e.printStackTrace();
-		    throw new InvalidArgumentException(i18n.tr("Unable to XXXX fetch data from: ") + 
-						       SWAMP.getInstance().getProperty("BUGZILLA_QUERYURL") + tokenvalue + "\n" + 
-						       i18n.tr("Reason: ") + e.getMessage());
-		}
-		// value is valid
-	    }
-	}
-        checkedValues.add(v);
-	    return v;
+        this.checkedValues.add(v);
+        return v;
     }
-    
+
 }
